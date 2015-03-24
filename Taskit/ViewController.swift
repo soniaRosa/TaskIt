64ce62b97e9752ef04f9abcae6495a61cbe4c5bf
Sónia Rosa <soniarosa@Air-de-Sonia.lan>
2015-03-24 18:40:46 -0100

    Initial Commit

---
+++ Taskit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taskit
+//
+//  Created by Sónia Rosa on 24/03/15.
+//  Copyright (c) 2015 Sónia Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
